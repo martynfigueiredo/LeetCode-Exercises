1184ab1e01ccfd1bb6df9fb60c5d52e17318a006
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-21 13:12:37 -0300

    Add project files.

---
+++ LeetCode Exercises/Solutions/Contains Duplicate II/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode_Exercises.Solutions.Contains_Duplicate_II
+{
+    public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+
+            if (nums == null || nums.Length < 1)
+                return false;
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+                else
+                {
+                    if (i - dic[nums[i]] <= k)
+                        return true;
+                    else
+                        dic[nums[i]] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
