28d1d61354aa33e53b11fcf9507469b8362c16f4
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-22 07:54:53 -0300

    Solution from https://www.youtube.com/watch?v=rLPRwRb4MUM

--- LeetCode Exercises/Solutions/Two Sums/Class.cs
+++ LeetCode Exercises/Solutions/Two Sums/Class.cs
@@ -4,24 +4,21 @@
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            var numsDictionary = new Dictionary<int, int>();
-            var complement = 0;
-            for (var i = 0; i < nums.Length; i++)
-            {
-                complement = target - nums[i];
-                var index = 0;
-                if (complement > 0 && numsDictionary.TryGetValue(complement, out index))
-                {
-                    return new int[] { index, i };
-                }
+            int[] result = new int[2];
 
-                if (numsDictionary.ContainsKey(nums[i]) == false)
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
                 {
-                    numsDictionary.Add(nums[i], i);
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
                 }
             }
-
-            return null;
+            return result;
         }
     }
 }
