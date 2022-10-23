c316590de270b0dd81d2a3bfce0986487cf4a3b7
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-23 09:29:00 -0300

    Solution

---
+++ LeetCode Exercises/Solutions/Set Mismatch/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeetCode_Exercises.Solutions.Set_Mismatch
+{
+    public class Solution
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            int result = -1;
+            int missingNumber = -1;
+            foreach (int num in nums)
+            {
+                if (nums[Math.Abs(num) - 1] < 0)
+                {
+                    result = Math.Abs(num);
+                }
+                else
+                {
+                    nums[Math.Abs(num) - 1] *= -1;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    result = i + 1;
+                }
+            }
+
+            return new int[] { result, missingNumber };
+        }
+    }
+}
