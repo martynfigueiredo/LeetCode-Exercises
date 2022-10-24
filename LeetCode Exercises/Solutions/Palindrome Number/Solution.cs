a54818482a27516f31642bed200e9292ad552e00
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-24 16:31:44 -0300

    Solution

--- LeetCode Exercises/Solutions/Palindrome Number/Solution.cs
+++ LeetCode Exercises/Solutions/Palindrome Number/Solution.cs
@@ -2,5 +2,30 @@
 {
     public class Solution
     {
+        public bool IsPalindrome(int x)
+        {
+
+            if (x < 0)
+            {
+                return false;
+            }
+            if (x < 10)
+            {
+                return true;
+            }
+
+            var tmp = x;
+            var comparison = 0;
+            var currentNumber = 0;
+
+            while (tmp != 0)
+            {
+                currentNumber = tmp % 10;
+                comparison = comparison * 10 + currentNumber;
+                tmp /= 10;
+            }
+
+            return x == comparison;
+        }
     }
 }
