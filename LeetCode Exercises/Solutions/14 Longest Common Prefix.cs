6f95cc6978e74c501c4c5489ec0aa83dac2d9b95
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-26 10:57:29 -0300

    Solution

--- LeetCode Exercises/Solutions/14 Longest Common Prefix.cs
+++ LeetCode Exercises/Solutions/14 Longest Common Prefix.cs
@@ -29,6 +29,21 @@ namespace LeetCode_Exercises.Solutions
 {
     public class _14_Longest_Common_Prefix
     {
-
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            }
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+            return prefix;
+        }
     }
 }
