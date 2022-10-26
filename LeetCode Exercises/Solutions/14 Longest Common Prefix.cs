e038efc1d0a2e0ce16c8ca22d585f7d99c99f171
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-26 10:56:45 -0300

    .

--- LeetCode Exercises/Solutions/14 Longest Common Prefix.cs
+++ LeetCode Exercises/Solutions/14 Longest Common Prefix.cs
@@ -1,4 +1,31 @@
-﻿namespace LeetCode_Exercises.Solutions
+﻿/***
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+
+ ***/
+
+namespace LeetCode_Exercises.Solutions
 {
     public class _14_Longest_Common_Prefix
     {
